72a1c6376fcac6899628546526b949daa6f6b0e3
Sabeur Thabti <thabti.saber@gmailcom>
2015-12-06 20:57:33 +0000

    heroku port

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 require("babel-core/register");
-app.set('port', (process.env.PORT || 3000));
 require.extensions['.scss'] = function () {
     return null
 };
 
 var express = require('express');
 var app = module.exports = express();
-
+pp.set('port', (process.env.PORT || 5000));
 app.set('views', require('path').join(__dirname, '/views'));
 app.engine('.hbs', require('express-handlebars')({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
