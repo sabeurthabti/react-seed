75c5404c43f6986c4e0c10a61daf8ef5f6626599
Sabeur Thabti <thabti.saber@gmailcom>
2015-12-06 20:48:12 +0000

    removing routes

--- server.js
+++ server.js
@@ -10,12 +10,18 @@ var app = module.exports = express();
 app.set('views', require('path').join(__dirname, '/views'));
 app.engine('.hbs', require('express-handlebars')({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
-
+console.log("HELLLLLLLLL");
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // for parsing application/json
 app.use('/assets', express.static('./assets'));
+console.log("HELLLLLLLLL222");
 
-require('./routes/index')(app);
+app.get('/', function(req, res) {
+
+res.send("hello");
+
+});
+
 
 app.listen(3000, function () {
     console.log('running on port 3000')
