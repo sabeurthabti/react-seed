a18b13f4a976545153d82d36d04e83405b9c6be3
Sabeur Thabti <thabti.sabeur@gmail.com>
2016-03-12 19:09:13 +0000

    bookmarklet

--- app/bookmarklet.js
+++ app/bookmarklet.js
@@ -2,7 +2,7 @@ javascript:(function(){
 
   if(typeof (window.jQuery || window.$) !== "undefined") {
     var script = document.createElement( 'script' );
-    script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
+    script.src = location.protocol +'//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
     script.onload=bookMarkIt;
     script.type = "text/javascript";
     document.body.appendChild(script);
